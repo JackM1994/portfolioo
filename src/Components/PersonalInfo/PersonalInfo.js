612078b09602a19cdf39bb666dd043c4d4000770
JackM1994 <36908600+JackM1994@users.noreply.github.com>
2020-06-05 16:03:55 +0100

    config file added

--- src/Components/PersonalInfo/PersonalInfo.js
+++ src/Components/PersonalInfo/PersonalInfo.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import './PersonalInfo.css';
-import LinkedIn from '../../assests/icons/linkedin.png';
-import Github from '../../assests/icons/logo.png';
-import Twitter from '../../assests/icons/twitter.png';
+import LinkedIn from '../../assests/icons/linkedin.svg';
+import Github from '../../assests/icons/logo.svg';
+import Twitter from '../../assests/icons/twitter.svg';
 
 const  personalInfo = () => {
     return(
@@ -17,10 +17,11 @@ const  personalInfo = () => {
                     I like to be constant with the customer so that we are both on
                     the same page and the finalised product is exactly what is needed to suit their needs.
                 </p>
+                <span className="social-media-icon"><img className="icon" src={LinkedIn} alt="linkedIn-icon"/></span>
+            <span className="social-media-icon"><img className="icon" src={Twitter} alt="twitter-icon"/></span>
+            <span className="social-media-icon"><img className="icon" src={Github} alt="github.icon"/></span>
             </div>
-            <span className="social-media-icon">{LinkedIn}</span>
-            <span className="social-media-icon">{Twitter}</span>
-            <span className="social-media-icon">{Github}</span>
+          
         </div>
     );
 }
